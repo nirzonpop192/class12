bad991a916d8751cc59d38407d4f2ba0aa696988
Faisal <nirzon192@gmail.com>
2021-02-05 16:21:08 +0600

    initial commit

---
+++ bin/class_12.dart
@@ -0,0 +1,39 @@
+
+
+
+void main(List<String> arguments) {
+
+ //*********** local pure  anonymous function**************
+  (){
+    var thisYear=2021;
+    int age=22;
+    var futureYear=2030;
+    int resultingAge=(futureYear-thisYear)+age;
+    print('S age will be: ${resultingAge}!');
+  }
+  // ******* calling pure anonymous function***********
+  .call();
+//********* calling global anonymous function ***********
+ // dim();
+
+}
+
+// anonymous global function
+var dim=(){
+  var thisYear=2021;
+  int age=22;
+  var futureYear=2030;
+  int resultingAge=(futureYear-thisYear)+age;
+  // return resultingAge;
+  print('S age will be: ${resultingAge}!');
+};
+
+// normal function
+void calculateSufiayanAge(){
+  var thisYear=2021;
+  int age=22;
+  var futureYear=2030;
+  int resultingAge=(futureYear-thisYear)+age;
+ // return resultingAge;
+  print('S age will be: ${resultingAge}!');
+}
---
+++ test/class_12_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_12/class_12.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
